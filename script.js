f35188cd5e70213e1c474d4a3820e3037bf911c8
sanishchirayath1 <sanishchirayath@gmail.com>
2021-11-13 11:37:47 +0530

    commit

--- script.js
+++ script.js
@@ -5,9 +5,10 @@ let firstArray = [1, 2, 3, 4]
 let secondArray = [5, 6, 7, 8]
 
 pushButton.addEventListener("click",() => {
-
+    secondArray.unshift(firstArray.shift())
 })
 
 popButton.addEventListener("click",() => {
-    
+    firstArray.push(secondArray.pop())
 })
+
