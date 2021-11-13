38ceafafac1471cd43784196b43a99d7404cf7fc
sanishchirayath1 <sanishchirayath@gmail.com>
2021-11-13 11:21:51 +0530

    commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const pushButton = document.getElementById("push")
+const popButton = document.getElementById("pop")
+
+let firstArray = [1, 2, 3, 4]
+let secondArray = [5, 6, 7, 8]
+
+pushButton.addEventListener("click",() => {
+
+})
+
+popButton.addEventListener("click",() => {
+    
+})
